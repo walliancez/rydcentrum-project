b850cdc048abea2283cd8ceb1bffc5240b25bc05
unknown <hanast130@E26649.edu.linkoping.se>
2015-04-16 09:33:33 +0200

    Whoops, forgot to add last commit

--- scripts/linker.js
+++ scripts/linker.js
@@ -16,6 +16,8 @@ function linker(){
 		console.log(filepath);
 		console.log(filename);
 	});
+
+	//Länkar knapparna i sidebaren till respektive sida
 	$('.menuitem').click(function(){
 		var itemID = $(this).attr('id');
 
@@ -24,6 +26,39 @@ function linker(){
 		switch (itemID){
 			case "home":
 				window.location.href = "./main.html";
+				break;
+			case "apoteket":
+				window.location.href = "./pages/apoteket.html";
+				break;
+			case "barbis":
+				window.location.href = "./pages/barb.html";
+				break;
+			case "blooming":
+				window.location.href = "./pages/blooming-design-(1).html";
+				break;
+			case "hemkop":
+				window.location.href = "./pages/hemk.html";
+				break;
+			case "ilpizza":
+				window.location.href = "./pages/ilpizza.html";
+				break;
+			case "lagpris":
+				window.location.href = "./pages/lagpris.html";
+				break;
+			case "linds":
+				window.location.href = "./pages/linds.html";
+				break;
+			case "sko":
+				window.location.href = "./pages/sko.html";
+				break;
+			case "stangastaden":
+				window.location.href = "./pages/stangastaden.html";
+				break;
+			case "thai":
+				window.location.href = "./pages/thai.html";
+				break;
+			case "tobak":
+				window.location.href = "./pages/tobak.html";
 				break;
 			default:
 				console.log("Shit went wrong");
